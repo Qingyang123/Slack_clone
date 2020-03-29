7077927eb7f49e33c831a047ad44ab4e944dc167
Qingyang Liu <qingyang@Qingyangs-MacBook-Pro.local>
2020-03-29 02:34:57 -0500

    First commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+
+import typeDefs from './schema';
+import resolvers from './resolvers';
+
+
+const PORT = 4000;
+
+const app = express();
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.applyMiddleware({ app });
+
+app.listen({ port: PORT }, () => {
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+});
