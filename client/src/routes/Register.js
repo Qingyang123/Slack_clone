6ba0289aba3a82c06fd0d425ad1407f967588570
Qingyang Liu <qingyang@Qingyangs-MacBook-Pro.local>
2020-03-31 01:12:05 -0500

    register form error handling

--- client/src/routes/Register.js
+++ client/src/routes/Register.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
-import { Container, Header, Input, Button } from 'semantic-ui-react';
+import { Container, Header, Form, Button } from 'semantic-ui-react';
 
 class Register extends Component {
     state = {
         username: '',
+        usernameError: '',
         email: '',
-        password: ''
+        emailError: '',
+        password: '',
+        passwordError: ''
     }
 
     onChange = (e) => {
@@ -17,23 +20,58 @@ class Register extends Component {
     }
 
     onSubmit = async () => {
-        console.log(this.state);
-        const res = await this.props.mutate({
-            variables: this.state
+        this.setState({
+            usernameError:"",
+            emailError: "",
+            passwordError: "",
         })
+        const { username, email, password } = this.state;
+        const res = await this.props.mutate({
+            variables: { username, email, password }
+        })
+
+        const { ok, errors } = res.data.register;
+        if (ok) {
+            this.props.history.push('/');
+        } else {
+            const err = {};
+            errors.forEach(({ path, message }) => {
+                err[`${path}Error`] = message;
+            })
+            this.setState(err);
+        }
         console.log(res);
     }
 
     render() {
 
-        const { username, email, password } = this.state;
+        const { username, email, password, usernameError, emailError, passwordError } = this.state
+
+        let errorList = {
+            usernameError: null,
+            emailError: null,
+            passwordError: null
+        };
+
+        if(usernameError) {
+            errorList['usernameError'] = usernameError
+        }
+        if(emailError) {
+            errorList['emailError'] = emailError
+        }
+        if(passwordError) {
+            errorList['passwordError'] = passwordError
+        }
+
         return (
             <Container text>
                 <Header as="h2">Register</Header>
-                <Input name='username' value={username} onChange={this.onChange} placeholder='Username' fluid/>
-                <Input name='email' value={email} onChange={this.onChange} placeholder='Email' fluid/>
-                <Input name='password' value={password} onChange={this.onChange} placeholder='Password' type='password' fluid/>
-                <Button onClick={this.onSubmit}>Submit</Button>
+                <Form>
+                    <Form.Input error={errorList['usernameError']} name='username' value={username} onChange={this.onChange} label='Username' placeholder='Username' fluid />
+                    <Form.Input error={errorList['emailError']} name='email' value={email} onChange={this.onChange} label='Email' placeholder='Email' fluid />
+                    <Form.Input error={errorList['passwordError']} name='password' value={password} onChange={this.onChange} label='Password' placeholder='Password' type='password' fluid />
+                    <Button onClick={this.onSubmit} primary>Submit</Button>
+                </Form>
             </Container>
         )
     }
@@ -41,7 +79,13 @@ class Register extends Component {
 
 const registerMutation = gql`
     mutation($username: String!, $email: String!, $password: String!) {
-        register(username: $username, email: $email, password: $password)
+        register(username: $username, email: $email, password: $password) {
+            ok
+            errors {
+                path
+                message
+            }
+        }
     }
 `;
 
--- server/resolvers/user.js
+++ server/resolvers/user.js
@@ -1,4 +1,14 @@
 import bcrypt from 'bcrypt';
+import Sequelize from 'sequelize';
+import _ from 'lodash'
+
+const formatErrors = (e, models) => {
+    if (e instanceof Sequelize.ValidationError) {
+        return e.errors.map(x => _.pick(x, ['path', 'message']));
+    }
+    return [{ path: 'name', message: 'something went wrong' }];
+};
+
 
 export default {
     Query: {
@@ -8,15 +18,27 @@ export default {
     Mutation: {
         register: async (parent, { ...args, password }, { models }) => {
             try {
+                if (password.length < 3 || password.length > 20) {
+                    return {
+                        ok: false,
+                        errors: [{path: 'password', message: 'The password needs to be between 3 and 20 characters long'}]
+                    }
+                }
                 const hashedPassword = await bcrypt.hash(password, 12);
-                await models.User.create({
+                const user = await models.User.create({
                     ...args, 
                     password: hashedPassword
                 })
-                return true;
+                return {
+                    ok: true,
+                    user
+                };
             } catch(err) {
                 console.log(err);
-                return false
+                return {
+                    ok: false,
+                    errors: formatErrors(err, models)
+                }
             }
         },
     }
