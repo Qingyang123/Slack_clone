7576f76eb36c334d47f9a9e9066b9f3cac19a727
Qingyang Liu <qingyang@Qingyangs-MacBook-Pro.local>
2020-04-11 18:44:19 -0500

    add graphql-redis-subscription

--- server/pubsub.js
+++ server/pubsub.js
@@ -1,3 +1,11 @@
-import { PubSub } from 'apollo-server';
+import { RedisPubSub } from 'graphql-redis-subscriptions';
 
-export default new PubSub();
+export default new RedisPubSub({
+    connection: {
+        host: '127.0.0.1',
+        port: 6379,
+        retry_strategy: options => {
+            return Math.max(options.attempt * 100, 3000);
+        }
+    }
+}); 
